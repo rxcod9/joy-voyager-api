9641176e15b1839c6e36e96003ad7504d990b8ee
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2021-06-25 02:49:04 +0530

    Api docs path

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -78,7 +78,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
             */
-            'docs' => storage_path('api-docs'),
+            'docs' => public_path('docs'),
 
             /*
              * Absolute path to directory where to export views
