ffacf260b047bc97b78acd2f93948844bfa1b014
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2021-06-24 12:18:33 +0530

    Fix swagger ui title

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'Joy Voyager Api Swagger UI',
             ],
 
             'routes' => [
