4d2c8da187463faa78611d15a42a9f3a0ba34278
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2021-07-10 11:40:18 +0530

    Fix Session store not set on request.

--- src/Http/Traits/CrudActions.php
+++ src/Http/Traits/CrudActions.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Joy\VoyagerApi\Http\Traits;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
+use TCG\Voyager\Http\Controllers\ContentTypes\File;
+use TCG\Voyager\Http\Controllers\ContentTypes\Relationship;
+
 trait CrudActions
 {
     use IndexAction;
@@ -14,4 +20,144 @@ trait CrudActions
     use StoreAction;
     use DestroyAction;
     use RestoreAction;
+
+    public function insertUpdateData($request, $slug, $rows, $data)
+    {
+        $multi_select = [];
+
+        // Pass $rows so that we avoid checking unused fields
+        $request->attributes->add(['breadRows' => $rows->pluck('field')->toArray()]);
+
+        /*
+         * Prepare Translations and Transform data
+         */
+        $translations = is_bread_translatable($data)
+                        ? $data->prepareTranslations($request)
+                        : [];
+
+        foreach ($rows as $row) {
+            // if the field for this row is absent from the request, continue
+            // checkboxes will be absent when unchecked, thus they are the exception
+            if (!$request->hasFile($row->field) && !$request->has($row->field) && $row->type !== 'checkbox') {
+                // if the field is a belongsToMany relationship, don't remove it
+                // if no content is provided, that means the relationships need to be removed
+                if (isset($row->details->type) && $row->details->type !== 'belongsToMany') {
+                    continue;
+                }
+            }
+
+            // Value is saved from $row->details->column row
+            if ($row->type == 'relationship' && $row->details->type == 'belongsTo') {
+                continue;
+            }
+
+            $content = $this->getContentBasedOnType($request, $slug, $row, $row->details);
+
+            if ($row->type == 'relationship' && $row->details->type != 'belongsToMany') {
+                $row->field = @$row->details->column;
+            }
+
+            /*
+             * merge ex_images/files and upload images/files
+             */
+            if (in_array($row->type, ['multiple_images', 'file']) && !is_null($content)) {
+                if (isset($data->{$row->field})) {
+                    $ex_files = json_decode($data->{$row->field}, true);
+                    if (!is_null($ex_files)) {
+                        $content = json_encode(array_merge($ex_files, json_decode($content)));
+                    }
+                }
+            }
+
+            if (is_null($content)) {
+                // If the image upload is null and it has a current image keep the current image
+                if ($row->type == 'image' && is_null($request->input($row->field)) && isset($data->{$row->field})) {
+                    $content = $data->{$row->field};
+                }
+
+                // If the multiple_images upload is null and it has a current image keep the current image
+                if ($row->type == 'multiple_images' && is_null($request->input($row->field)) && isset($data->{$row->field})) {
+                    $content = $data->{$row->field};
+                }
+
+                // If the file upload is null and it has a current file keep the current file
+                if ($row->type == 'file') {
+                    $content = $data->{$row->field};
+                    if (!$content) {
+                        $content = json_encode([]);
+                    }
+                }
+
+                if ($row->type == 'password') {
+                    $content = $data->{$row->field};
+                }
+            }
+
+            if ($row->type == 'relationship' && $row->details->type == 'belongsToMany') {
+                // Only if select_multiple is working with a relationship
+                $multi_select[] = [
+                    'model'           => $row->details->model,
+                    'content'         => $content,
+                    'table'           => $row->details->pivot_table,
+                    'foreignPivotKey' => $row->details->foreign_pivot_key ?? null,
+                    'relatedPivotKey' => $row->details->related_pivot_key ?? null,
+                    'parentKey'       => $row->details->parent_key ?? null,
+                    'relatedKey'      => $row->details->key,
+                ];
+            } else {
+                $data->{$row->field} = $content;
+            }
+        }
+
+        if (isset($data->additional_attributes)) {
+            foreach ($data->additional_attributes as $attr) {
+                if ($request->has($attr)) {
+                    $data->{$attr} = $request->{$attr};
+                }
+            }
+        }
+
+        $data->save();
+
+        // Save translations
+        if (count($translations) > 0) {
+            $data->saveTranslations($translations);
+        }
+
+        foreach ($multi_select as $sync_data) {
+            $data->belongsToMany(
+                $sync_data['model'],
+                $sync_data['table'],
+                $sync_data['foreignPivotKey'],
+                $sync_data['relatedPivotKey'],
+                $sync_data['parentKey'],
+                $sync_data['relatedKey']
+            )->sync($sync_data['content']);
+        }
+
+        // FIX: Session store not set on request issue
+        // Rename folders for newly created data through media-picker
+        if (Session::has($slug . '_path') || Session::has($slug . '_uuid')) {
+            $oldPath    = Session::get($slug . '_path');
+            $uuid       = Session::get($slug . '_uuid');
+            $newPath    = str_replace($uuid, $data->getKey(), $oldPath);
+            $folderPath = substr($oldPath, 0, strpos($oldPath, $uuid)) . $uuid;
+
+            $rows->where('type', 'media_picker')->each(function ($row) use ($data, $uuid) {
+                $data->{$row->field} = str_replace($uuid, $data->getKey(), $data->{$row->field});
+            });
+            $data->save();
+            if (
+                $oldPath != $newPath &&
+                !Storage::disk(config('voyager.storage.disk'))->exists($newPath) &&
+                Storage::disk(config('voyager.storage.disk'))->exists($oldPath)
+            ) {
+                Session::forget([$slug . '_path', $slug . '_uuid']);
+                Storage::disk(config('voyager.storage.disk'))->move($oldPath, $newPath);
+                Storage::disk(config('voyager.storage.disk'))->deleteDirectory($folderPath);
+            }
+        }
+
+        return $data;
+    }
 }
