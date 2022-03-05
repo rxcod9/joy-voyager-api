75b8e1dee329a22a51adfbfe3daa8519d7323194
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2022-03-05 21:23:55 +0530

    🐛 Fix swagger issue

--- src/Http/Controllers/VoyagerBaseController.php
+++ src/Http/Controllers/VoyagerBaseController.php
@@ -8,27 +8,26 @@ use Illuminate\Support\Facades\Auth;
 use Joy\VoyagerApi\Http\Traits\CrudActions;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController as TCGVoyagerBaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title=APP_NAME,
+ *      description="Joy VoyagerApi module adds REST Api end points to Voyager with Passport and Swagger support https://github.com/rxcod9/joy-voyager-api."
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Primary API Server"
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST2,
+ *      description="Another API Server"
+ * )
+ */
 class VoyagerBaseController extends TCGVoyagerBaseController
 {
     use CrudActions;
-
-    /**
-     * @OA\Info(
-     *      version="1.0.0",
-     *      title=APP_NAME,
-     *      description="Joy VoyagerApi module adds REST Api end points to Voyager with Passport and Swagger support https://github.com/rxcod9/joy-voyager-api."
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST,
-     *      description="Primary API Server"
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST2,
-     *      description="Another API Server"
-     * )
-     */
 
     /**
      * Create a new instance
