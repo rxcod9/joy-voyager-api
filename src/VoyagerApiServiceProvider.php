2eb73819bff86b92cbb37e124ab73aa56ef8adae
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2023-01-22 21:56:47 +0530

    Manage autoload migrations

--- src/VoyagerApiServiceProvider.php
+++ src/VoyagerApiServiceProvider.php
@@ -35,7 +35,9 @@ class VoyagerApiServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        if (config('joy-voyager-api.database.autoload_migrations', true)) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
 
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'joy-voyager-api');
     }
